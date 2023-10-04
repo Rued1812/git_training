d0afdf9183b633d70ceb614ee2fcaff106b48d96
Rued1812 <145365936+Rued1812@users.noreply.github.com>
2023-10-04 14:30:18 +0200

    Update test_packages.R

--- test_packages.R
+++ test_packages.R
@@ -1,7 +1,2 @@
 library(RPostgres)
-<<<<<<< HEAD
-library(tidyverse) # Often usen in data science
-
-=======
-library(tidyverse) # load many packages
->>>>>>> 9f27dbace958c6c46bd02a97e625f80c0411afa0
+library(tidyverse)
